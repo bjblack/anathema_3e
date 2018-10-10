3053dd96d9f44427faafd35b3364efcaf67035e9
BJ <BJ@BJ-PC>
2018-10-09 18:25:09 -0800

    Test Commit

--- Anathema/src/main/java/net/sf/anathema/AnathemaBootLoader.java
+++ Anathema/src/main/java/net/sf/anathema/AnathemaBootLoader.java
@@ -8,42 +8,65 @@ import java.util.Properties;
 
 public class AnathemaBootLoader {
 
-  public static void main(String[] arguments) throws Exception {
-    ClassLoader loader = selectClassLoader();
-    Class<?> mainClass = loadMainClass(loader);
-    Object instance = mainClass.newInstance();
-    Method method = mainClass.getMethod("startApplication");
-    method.invoke(instance);
-  }
+	public static void main (String[] arguments) throws Exception
+	{
+		
+		ClassLoader loader = selectClassLoader ();
+		Class<?> mainClass = loadMainClass (loader);
+		Object instance = mainClass.newInstance ();
+		
+		Method method = mainClass.getMethod ("startApplication");
+		// This is a method of the Anathema class, defined in the Platform_FX project.
+		
+		method.invoke (instance);
+		
+	}
 
-  private static ClassLoader selectClassLoader() throws IOException {
-    if (!isClasspathConfigured()) {
-      return useCustomClassLoader();
-    } else {
-      return useSystemClassLoader();
-    }
-  }
+	private static ClassLoader selectClassLoader () throws IOException
+	{
+		
+		// Returns the system class loader if it will work; returns a custom loader if the system loader excepts out.
+		if (isClasspathConfigured ()) {
+			return useSystemClassLoader ();
+		} else {
+			return useCustomClassLoader ();
+		}
+		
+	}
 
-  private static ClassLoader useSystemClassLoader() throws IOException {
-    return ClassLoader.getSystemClassLoader();
-  }
+	private static ClassLoader useSystemClassLoader () throws IOException
+	{
+		
+		return ClassLoader.getSystemClassLoader ();
+		
+	}
 
-  private static ClassLoader useCustomClassLoader() throws MalformedURLException {
-    Properties properties = new PropertiesLoader("anathema.properties").load();
-    String libraryFolder = properties.getProperty("library.folder");
-    return new EasyLoader(Paths.get(libraryFolder));
-  }
+	private static ClassLoader useCustomClassLoader () throws MalformedURLException
+	{
+		
+		Properties properties = new PropertiesLoader ("anathema.properties").load ();
+		String libraryFolder = properties.getProperty ("library.folder");
+		return new EasyLoader (Paths.get (libraryFolder));
+		
+	}
 
-  private static boolean isClasspathConfigured() {
-    try {
-      loadMainClass(ClassLoader.getSystemClassLoader());
-      return true;
-    } catch (ClassNotFoundException e) {
-      return false;
-    }
-  }
+	private static boolean isClasspathConfigured ()
+	{
+		
+		// Tests the system class loader. If loading class "net.sf.anathema.Anathema" throws the exception, the classpath is not configured.
+		try {
+			loadMainClass (ClassLoader.getSystemClassLoader ());
+			return true;
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+		
+	}
 
-  private static Class<?> loadMainClass(ClassLoader loader) throws ClassNotFoundException {
-    return loader.loadClass("net.sf.anathema.Anathema");
-  }
+	private static Class<?> loadMainClass (ClassLoader loader) throws ClassNotFoundException
+	{
+		
+		return loader.loadClass ("net.sf.anathema.Anathema");
+		
+	}
 }
